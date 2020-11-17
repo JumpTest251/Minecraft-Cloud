0fb30046dbc9c9f42b37fa22809b90b5a19e12df
Jumper251 <a.a.m.2502@googlemail.com>
2020-11-17 23:39:07 +0100

    cache fix

--- server-provisioning/routes/backup.js
+++ server-provisioning/routes/backup.js
@@ -1,7 +1,7 @@
 const ServerTemplate = require('../models/ServerTemplate');
 const gcloud = require('../provisioning/gcloud')
 const Joi = require('joi');
-const { Cache } = require('../utils/cache');
+const { Cache } = require('../utils/caching');
 
 const generationRule = Joi.number().integer().positive().required();
 
