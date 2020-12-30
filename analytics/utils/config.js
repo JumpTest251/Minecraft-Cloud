bbab250b7cd14a2fb29f79efcd73cbae4aaf53c7
Jumper251 <a.a.m.2502@googlemail.com>
2020-12-30 16:07:58 +0100

    fix url

--- analytics/utils/config.js
+++ analytics/utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoUrl: process.env.MONGO_URL,
-    serverServiceUrl: (process.env.NODE_ENV === 'production' ? "https://" : "http://") + process.env.SERVER_SERVICE + ":" + process.env.SERVER_SERVICE_PORT,
+    serverServiceUrl: (process.env.NODE_ENV === 'production' ? "http://" : "http://") + process.env.SERVER_SERVICE + ":" + process.env.SERVER_SERVICE_PORT,
     redisUrl: process.env.REDIS_URL,
     influxToken: process.env.INFLUX_TOKEN,
     influxOrg: process.env.INFLUX_ORG,
